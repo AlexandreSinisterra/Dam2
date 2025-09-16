7952ecae7bf8b1f77fde83eea8c8f16917ad9fd0
dam <agarciasinisterra@danielcastelao.org>
2025-09-16 09:24:01 +0200

    a

---
+++ DI/tiposEnumeros.py
@@ -0,0 +1,16 @@
+numEnteiro = 5
+print(type(numEnteiro))
+
+numDecimal = 5.6
+numDecimal2 = 0.1e-5
+print(type(numDecimal))
+print(type(numDecimal2))
+print(10/3)
+
+booleano = True
+print(type(booleano))
+
+cadea = "Esto es una cadea con comillas 'dobres'"
+cadea2 = 'Esto es una cadea con comillas "dobres"'
+print(type(cadea))
+print(type(cadea2))
