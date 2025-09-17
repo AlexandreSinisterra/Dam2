9006fade76e5e56f4070abe1ff28abc37babf54c
dam <agarciasinisterra@danielcastelao.org>
2025-09-17 13:12:54 +0200

    focas

--- PMDM/AquariumFish.kt
+++ PMDM/AquariumFish.kt
@@ -32,6 +32,18 @@ class Shark: FishAction, FishColor {
     }
 }
 
+sealed class Seal
+class SeaLion : Seal()
+class Walrus : Seal()
+
+fun matchSeal(seal: Seal): String {
+    return when(seal) {
+        is Walrus -> "walrus"
+        is SeaLion -> "sea lion"
+    }
+}
+
+
 class Plecostomus (fishColor: FishColor = GoldColor):
     FishAction by PrintingFishAction("eat algae"),
     FishColor by fishColor
@@ -42,6 +54,14 @@ class PrintingFishAction(val food: String) : FishAction {
     }
 }
 
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+}
+
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+}
+
 object GoldColor : FishColor {
     override val color = "gold"
 }
@@ -49,14 +69,19 @@ object GoldColor : FishColor {
 fun makeFish() {
     val shark = Shark()
     val pleco = Plecostomus()
+    val seal: Seal = SeaLion()
     println("Shark: ${shark.color}")
     shark.eat()
     println("Plecostomus: ${pleco.color}")
     pleco.eat()
+    println("Seal: ${matchSeal(seal)}")
 }
 
 fun main () {
     makeFish()
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
 }
 
 
