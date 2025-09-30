63683c35d6d82509178deec440e6ce1c1e83fffa
dam <agarciasinisterra@danielcastelao.org>
2025-09-30 08:24:23 +0200

    readme

---
+++ out/production/Dam2/quieroIrmeACasa.py
@@ -0,0 +1,21 @@
+n1 = 3
+
+if n1 < 5:
+    print("a")
+    print("a")
+    print("a")
+else:
+    print("o")
+
+while True:
+    print(n1)
+    n1+=1
+    if n1== 10:
+        print("true")
+        print(n1)
+        break
+    else:
+        print("false")
+
+
+
