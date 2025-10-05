47c4d8296cd22b55891e47d7739a39ef2aa05026
AlexandreSinisterra <agarciasinisterra@danielcastelao.org>
2025-10-05 20:58:20 +0200

    añadido precio a los cafes

--- PMDM/MaquinaDeCafe/Cafe.kt
+++ PMDM/MaquinaDeCafe/Cafe.kt
@@ -6,10 +6,11 @@ abstract class Cafe(
     val cafeNecesario: Int,
     val azucarNecesaria: Int,
     val lecheNecesaria: Int,
+    val precio: Int,
     val vasoNecesario: Int = 1,
     val palitoNecesario: Int = 1
 )
 
-class Americano : Cafe("Americano", 200, 15, 5, 0)
-class Africano : Cafe("Africano", 0, 20, 7, 10)
-class Capuchino : Cafe("Capuchino", 100, 15, 5, 50)
+class Americano : Cafe("Americano", 200, 15, 5, 0, 2)
+class Africano : Cafe("Africano", 0, 20, 7, 10, 5)
+class Capuchino : Cafe("Capuchino", 100, 15, 5, 50, 3)
