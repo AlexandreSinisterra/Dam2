da6aa71db3bce1ab06b1b21f29ecf4a80ccb65e4
dam <agarciasinisterra@danielcastelao.org>
2025-10-07 13:28:31 +0200

    Parte 1 y 2, CBD AD

--- DA/P2_1/P2_1_CBD.java
+++ DA/P2_1/P2_1_CBD.java
@@ -2,6 +2,7 @@ package P2_1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class P2_1_CBD {
@@ -10,7 +11,15 @@ public class P2_1_CBD {
     private static final String contraseña = "admin";
 
     public static void main(String[] args) throws SQLException {
-        crearInsertar(conexion());
+        Connection bD = conexion();
+        crear(bD);
+        insertar(bD);
+        select(bD);
+        insertar2(bD);
+        select2(bD);
+        update(bD);
+        delete(bD);
+        select(bD);
     }
 
     static Connection conexion() throws SQLException {
@@ -26,7 +35,7 @@ public class P2_1_CBD {
         return prueba1;
     }
 
-    public static void crearInsertar(Connection prueba1) throws SQLException {
+    public static void crear(Connection prueba1) throws SQLException {
         String sql = "CREATE TABLE anime (" +
                 "nome VARCHAR(100), " +
                 "descripcion TEXT, " +
@@ -34,11 +43,51 @@ public class P2_1_CBD {
                 "puntuacion INTEGER" +
                 ")";
         prueba1.createStatement().execute(sql);
-        sql = "INSERT INTO anime (nome, descripcion, data, puntuacion) VALUES " +
+    }
+
+    public static void insertar(Connection prueba1) throws SQLException {
+        String sql = "INSERT INTO anime (nome, descripcion, data, puntuacion) VALUES " +
                 "('Evangelion', 'Serie de mechas que explora as emocións dos pilotos nunha ameaza global apocalíptica.', '1995-10-04', 95)," +
                 "('Ghost In the Shell', 'Anime de ciencia ficción cibernética sobre intelixencia artificial e a identidade.', '1995-11-18', 92)," +
                 "('Akira', 'Película postapocalíptica con acción e crítica social ambientada nunha Tokio futurista.', '1988-07-16', 90)," +
                 "('Dragon Ball', 'Serie clásica de aventuras e artes marciais con personaxes icónicos e épicos combates.', '1986-02-26', 88);";
-        prueba1.createStatement().execute(sql);
+        prueba1.createStatement().executeUpdate(sql);
     }
+
+    public static void select(Connection prueba1) throws SQLException {
+        String sql = "select * from anime";
+        ResultSet salida = prueba1.createStatement().executeQuery(sql);
+        while (salida.next()) {
+            System.out.println("");
+            System.out.println("Nombre: " + salida.getString("nome"));
+            System.out.println("Descripcion: " + salida.getString("descripcion"));
+            System.out.println("Fecha: " + salida.getDate("data"));
+            System.out.println("Puntuacion: " + salida.getInt("puntuacion"));
+            System.out.println("");
+        }
+    }
+
+    public static void insertar2(Connection prueba1) throws SQLException {
+        String sql = "INSERT INTO anime (nome, descripcion, data, puntuacion) VALUES ('DevilMan Crybaby', 'Serie de una Profecia apocaliptica de demonios', '2018-01-05', 100);";
+        prueba1.createStatement().executeUpdate(sql);
+    }
+
+    public static void select2(Connection prueba1) throws SQLException {
+        String sql = "select * from anime where puntuacion = '100'";
+        ResultSet salida = prueba1.createStatement().executeQuery(sql);
+        while (salida.next()){
+            System.out.println("Nome: " + salida.getString("nome"));
+        }
+    }
+
+    public static void update(Connection prueba1) throws SQLException {
+        String sql = "update anime set puntuacion = 95 where nome = 'DevilMan Crybaby'";
+        prueba1.createStatement().executeUpdate(sql);
+    }
+
+    public static void delete(Connection prueba1) throws SQLException {
+        String sql = "delete from anime where nome = 'Dragon Ball'";
+        prueba1.createStatement().executeUpdate(sql);
+    }
+
 }
